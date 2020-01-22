14d035f12da98bed7cfa4ea9623fcfd897d1e112
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-22 23:25:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 class Dog
+  attr_accessor :name, :breed
+  
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
  
-  def name=(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
+  # def name=(name, breed = "Mutt")
+  #   @name = name
+  #   @breed = breed
+  # end
   def name
     @name
   end
